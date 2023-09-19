0a8b00dcb69ca9a9de76fa642b1dcfa36319c02c
Sai Vignesh <sai3125.be@gmail.com>
2023-09-19 19:28:01 +0530

    Edit Method

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 
 include 'DbConnect.php';
 $objDb = new DbConnect;
@@ -12,9 +13,20 @@ try {
     switch($method){
         case "GET":
             $sql = "SELECT * FROM users";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $path = explode('/',$_SERVER['REQUEST_URI']);
+            if(isset($path[3]) && is_numeric($path[3])) {
+                $sql .= " WHERE id = :id";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':id',$path[3]);
+                $stmt->execute();
+                $users = $stmt->fetch(PDO::FETCH_ASSOC);
+            } 
+            else {
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+            
             $response = $users;
             break;
 
@@ -32,6 +44,24 @@ try {
             }
             else{
                 $response = ['status' => 0, 'message' => 'Failed'];
+            }
+            break;
+
+        case "PUT":
+            $user = json_decode(file_get_contents('php://input'));
+            $sql = "UPDATE users SET name= :name, email = :email, mobile = :mobile, update_at= :update_at WHERE id= :id";
+            $stmt = $conn->prepare($sql);
+            $created_at = date('Y-m-d');
+            $stmt->bindParam(':id',$user->id);
+            $stmt->bindParam(':name',$user->name);
+            $stmt->bindParam(':email',$user->email);
+            $stmt->bindParam(':mobile',$user->mobile);
+            $stmt->bindParam(':update_at',$user->update_at);
+            if($stmt->execute()){ // Corrected 'executed()' to 'execute()'
+                $response = ['status' => 1, 'message' => 'Updated'];
+            }
+            else{
+                $response = ['status' => 0, 'message' => 'Failed to Update'];
             }
             break;
     }
